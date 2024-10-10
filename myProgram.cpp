a6353213f45c553d2d4b58244836821b3eca06c0
Daniel Carrizo <danjo2c@gmail.com>
2024-10-10 09:57:02 -0400

    feat: new fuction

--- myProgram.cpp
+++ myProgram.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
+#include "function.h"
 using namespace std;
 
 int main(){
     int x =0;
     cout<<"Hello world in git"<<endl;
     cin>>x;
-    cout<<"this is your number "<<x<<endl;
+    cout<<"this is your number "<<sum(4,x)<<endl;
     return 0;
 }
